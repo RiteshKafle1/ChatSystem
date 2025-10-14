7a41d8cf463b0d38aa775154a5e9ee23b5906b25
Ritesh Kafle1 <riteshkafle10@gmail.com>
2025-10-14 11:32:47 +0545

    user model created

---
+++ Backend/models/user.model.js
@@ -0,0 +1,30 @@
+import mongoose from "mongoose";
+
+const userSchema = new mongoose.Schema(
+  {
+    email: {
+      type: String,
+      required: true,
+      unique: true,
+    },
+    fullName: {
+      type: String,
+      required: true,
+      // unique:true,
+    },
+    password: {
+      type: String,
+      required: true,
+      // unique:true,
+    },
+    profilePic: {
+      type: String,
+      default: "",
+      // required:true,
+      // unique:true,
+    },
+  },
+  { timestamps: true }
+);
+const User = mongoose.models.User || mongoose.model("User", userSchema);
+export default User;
