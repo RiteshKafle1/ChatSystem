4f3f9f018973ce2aa199bd8a798ac84498ae792d
Ritesh Kafle1 <riteshkafle10@gmail.com>
2025-10-18 11:35:49 +0545

    implemented both token access and refresh

--- Backend/utils/token.util.js
+++ Backend/utils/token.util.js
@@ -2,14 +2,22 @@ import jwt from "jsonwebtoken";
 import { ENV } from "../config/.env.js";
 
 export const generateToken = (userId, res) => {
-  const token = jwt.sign({ userId }, ENV.JWT_SECRET, {
+  const accessToken = jwt.sign({ userId }, ENV.JWT_ACCESS_SECRET, {
+    expiresIn: "15m",
+  });
+  const refreshToken = jwt.sign({ userId }, ENV.JWT_REFRESH_SECRET, {
     expiresIn: "7d",
   });
 
-  res.cookie("jwt", token, {
+  res.cookie("accessToken", accessToken, {
+    maxAge: 15 * 60 * 100,
+    httpOnly: true,
+    sameSite: "strict",
+  });
+  res.cookie("refreshToken", refreshToken, {
     maxAge: 7 * 24 * 60 * 60 * 1000,
     httpOnly: true,
     sameSite: "strict",
   });
-  return token;
+  return { accessToken, refreshToken };
 };
